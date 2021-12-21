78c7f8e853125a9a30ef470007e42dc5f2a8563c
kuaniscreative <benben19911020@gmail.com>
2021-12-21 16:18:58 +0800

    feat(app): clean up create-next-app default settings and files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import 'modern-normalize';
+import '@@ui/global.scss';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
